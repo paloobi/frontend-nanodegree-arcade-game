00cdc1b7f12147f3e3fb06b0c635b196f0737465
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2015-10-16 19:34:05 -0700

    add comments and README file

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
-frontend-nanodegree-arcade-game
-===============================
+Bugger
+======
 
-Students should use this rubric: https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797
+This game was created as an assignment for Udacity's Front-End Web Development Nanodegree.
 
-for self-checking their submission.
+How to Run the App
+------------------
+
+  1. Download the contents (either using git clone from github, or by downloading a Zip file).
+
+  2. Open index.html in your web browser.
+
+How to Play
+-----------
+
+To play the game, move the Player using the Left, Right, Up and Down arrow keys.
+
+Try to get the Player to reach the water without touching any Bugs. If you touch a Bug, you will lose a point and your character will go back to the start!
+
+If you reach the water, you get 1 point! Yay! You're a winner! Goal of the game is to win as many times are you can!
+
+Have fun!
--- js/app.js
+++ js/app.js
@@ -6,9 +6,13 @@ var Enemy = function(row, speed) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
     this.x = 0;
+    // sets the y value, based on specified row
     this.y = row * 75;
     this.speed = speed;
+
+    // the width and height will be used for collision detection
     this.width = 100;
     this.height = 60;
 };
@@ -16,13 +20,14 @@ var Enemy = function(row, speed) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
+    // Multipling  movement by the dt parameter
+    // to ensure the game runs at the same speed for
     // all computers.
     this.x += this.speed * dt;
 
-    //retrieve player variables to create a player rectangle
-    //these values are then used for collision detection
+    // retrieve player variables to create a player rectangle
+    // these values are used for collision detection
+    // x, y coordinates needed to be adjusted for proper use in rectangle
     var playerX = player.getX() + 20;
     var playerY = player.getY() + 60;
     var playerWidth = player.getWidth();
@@ -33,6 +38,8 @@ Enemy.prototype.update = function(dt) {
         this.x + this.width > playerX &&
         this.y + 80 < playerY + playerHeight &&
         this.height + this.y + 80 > playerY) {
+        
+        // if so, trigger a loss and move Player to starting position
         player.reset("loss");
     }
 
@@ -43,126 +50,177 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
+// Player that is controlled by the site visitor
 var Player = function() {
     this.sprite = 'images/char-boy.png';
+
+    // initial x, y coordinates
     this.x = 200;
     this.y = 400;
+
+    // width & height for use in collision detection
     this.width = 60;
     this.height = 80;
 
-    this.direction = "none"
+    // without any commands given, direction will start without a value
+    this.direction = "none";
+
+    // start the player with no wins or losses
+    // these will increment as the game continues
     this.wins = 0;
     this.losses = 0;
 };
 
 Player.prototype.update = function(dt) {
+    // use the direction to move the player
     switch (this.direction) {
+
+        // if the direction is left, move Player to the left one slot
+        // unless Player is too far to the left
         case 'left':
             if (this.x >= 20) {
                 this.x -= 100;
             }
             break;
+
+        // if the direction is up, move Player up one slot
         case 'up':
             this.y -= 80;
             break;
+
+        // if the direction is right, move Player right one slot
+        // unless player is too far to the right already
         case 'right':
             if (this.x <= 350) {
                 this.x += 100;
             }
             break;
+
+        // if direction is down, move Player down one slot
+        // unless player is too far down already
         case 'down':
             if (this.y <= 360) {
                 this.y += 80;
             }
             break;
     }
+    // reset the direction to none
     this.direction = "none";
+
+    // if the player has reached the water,
+    // call the reset function, and trigger a win
     if (this.y <= 70) {
         this.reset("win");
     }
 };
 
+// renders the player on the screen
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// sets the player's direction based on the key that was pressed
 Player.prototype.handleInput = function(keyCode) {
     this.direction = keyCode;
 };
 
+// returns player's x coordinate
+// for use in collision detection by Enemy instances
 Player.prototype.getX = function() {
-// returns an array containing y coordinate of player
     return this.x;
 }
 
+// returns player's y coordinate
+// for use in collision detection by Enemy instances
 Player.prototype.getY = function() {
-// returns an array containing y coordinate of player
     return this.y;
 }
 
+// returns player's width property,
+// for use in collision detection by Enemy instances
 Player.prototype.getWidth = function() {
     return this.width;
 }
 
+// returns plater's height property,
+// for use in collision detection by Enemy instances
 Player.prototype.getHeight = function() {
     return this.height;
 }
 
+// Returns player to starting location
+// Input: outcomt - "loss" or "win"
 Player.prototype.reset = function(outcome) {
+    // starting x,y coordinates
     this.x = 200;
     this.y = 400;
+
+    // if outcome is loss, subtract 1 from the loss count
     if (outcome == "loss") {
         this.losses -= 1;
+    // if the outcome is a win, add 1 to the win count
     } else if (outcome == "win") {
         this.wins += 1;
     }
 };
 
+// displays the player's current scores
 Player.prototype.renderScores = function() {
+
+    // create a white circle in the top left corner
     ctx.fillStyle = "white";
     ctx.arc(50,91,20,0*Math.PI,2*Math.PI)
     ctx.fill();
 
-
+    // create a white circle in the top right corner
     ctx.fillStyle = "white";
     ctx.arc(460,91,20,0*Math.PI,2*Math.PI)
     ctx.fill();
 
+    // set font properties
     ctx.font = "24px Arial";
     ctx.textAlign = "center"
+
+    // display player losses in red in the upper left corner
     ctx.fillStyle = "red";
     ctx.fillText(player.losses,50,100);
+
+    // display player wins in black in the upper right corner
     ctx.fillStyle = "black";
     ctx.fillText(player.wins,460,100);
 }
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// ---------- instantiate player and enemy objects ------------------
 
+
+
+
+// All enemy objects are added to an array called allEnemies
 var allEnemies = [];
 
+// spawn enemies at a regular interval
 var myVar;
-
 function spawnEnemies() {
     myVar = setInterval(enemySpawner, 1000);
 }
 
+// function for creating a random number within a range
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// function for spawning enemies in a random row and at a random speed (within accepted speeds)
 function enemySpawner() {
     var row = getRandomInt(1,3);
     var speed = getRandomInt(150,250);
     allEnemies.push(new Enemy(row, speed));
 };
 
+// Begin spawning enemies every interval
 spawnEnemies();
 
+
+// player object saved in a variable called player
 var player = new Player();
 
 
